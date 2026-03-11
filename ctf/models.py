70d4d5f8c126c5e0a2663989e2007deff67397f9
Marc Olivier Bergeron <mbergeron28@proton.me>
2026-03-11 16:36:35 -0400

    Added services validation

--- ctf/models.py
+++ ctf/models.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
-from typing import Annotated, Any
+from typing import Annotated, Any, Literal
 
 from pydantic import (
     BaseModel,
+    ConfigDict,
+    Field,
+    RootModel,
     StringConstraints,
 )
 
 IncusStr = Annotated[str, StringConstraints(pattern=r"^[a-z][a-z0-9\-]{0,61}[a-z0-9]$")]
+PortNumber = Annotated[int, Field(ge=1, le=65535)]
+CheckType = Literal["http", "https", "ssh", "tcp"]
 
 
 class Track(BaseModel):
@@ -49,3 +54,92 @@ class ValidationError(BaseModel, frozen=True):
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(error_name="{self.error_name}", error_description="{self.error_description}", track_name="{self.track_name}", details= {self.details})'
+
+
+class TrackContacts(BaseModel):
+    dev: list[str]
+    qa: list[str]
+    support: list[str]
+
+
+class InstanceConfig(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
+
+class InstanceDeviceProperties(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
+
+class InstanceDevice(BaseModel):
+    name: str
+    type: str
+    properties: InstanceDeviceProperties
+
+
+class InstanceWaitFor(BaseModel):
+    model_config = ConfigDict(extra="allow")
+    type: str | None = None
+
+
+class InstanceService(BaseModel):
+    name: str
+    port: PortNumber
+    check: CheckType
+    dev_port_mapping: PortNumber | None = None
+
+
+class TrackInstance(BaseModel):
+    is_build_container: bool = False
+    image: str
+    profiles: list[str]
+    type: Literal["container", "virtual-machine"]
+    description: str
+    hwaddr: str | None = None
+    record: str | None = None
+    ipv6: str | None = None
+    config: InstanceConfig
+    devices: list[InstanceDevice]
+    wait_for: InstanceWaitFor | None = None
+    services: list[InstanceService]
+
+
+class TrackInstances(RootModel[dict[str, TrackInstance]]): ...
+
+
+class FlagTags(BaseModel):
+    model_config = ConfigDict(extra="allow")
+    discourse: str | None = None
+    ui_sound: str | None = None
+    ui_gif: str | None = None
+
+
+class TrackFlag(BaseModel):
+    flag: str
+    value: int
+    description: str | None = None
+    return_string: str
+    cfss: str | None = None
+    tags: FlagTags | None = None
+
+
+class DeprecatedTrackService(BaseModel):
+    name: str
+    instance: str
+    address: str
+    port: PortNumber
+    check: CheckType
+    dev_port_mapping: PortNumber | None = None
+
+
+class TrackYaml(BaseModel):
+    name: str
+    description: str
+    integrated_with_scenario: bool
+    contacts: TrackContacts
+    instances: TrackInstances | None = None
+    flags: list[TrackFlag]
+    services: list[DeprecatedTrackService] | None = None
+
+
+# TrackInstances.model_rebuild()
+# TrackYaml.model_rebuild()
